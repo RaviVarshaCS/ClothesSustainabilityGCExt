48b7f755dc6def4e7bd127ed0700865e864411b7
hsiddharthan <115902651+hsiddharthan@users.noreply.github.com>
2022-10-16 03:14:55 -0500

    Add files via upload

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 // Send a message containing the page details back to the event page
 chrome.runtime.sendMessage({
-    'title': document.title,
-    'url': window.location.href,
-    'summary': window.getSelection().toString()
+    'productName': "pants",
+    'susIndex': 8,
+    'infoLink': "https://oldnavy.gap.com/"
 });
